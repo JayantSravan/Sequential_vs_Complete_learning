b2e61a848ec9752704588d3742eeb4bf42735afc
JayantSravan <jayant.sravan@gmail.com>
2018-09-23 13:25:58 +0530

    Started with the project

---
+++ run.py
@@ -0,0 +1,11 @@
+"""
+This project aims to see the difference between sequential learning and
+complete learning. We work on the coin tossing problem.
+"""
+import numpy as np
+import scipy as sp
+import matplotlib
+
+class Data:
+    def generate(self, mean, numOfPoints):
+        pass
