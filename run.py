5baf13370994a3206fed3b5ab5fa4cfd6b4c2772
JayantSravan <jayant.sravan@gmail.com>
2018-09-24 21:20:08 +0530

    Working moving sine curve

--- run.py
+++ run.py
@@ -4,7 +4,11 @@ complete learning. We work on the coin tossing problem.
 """
 import numpy as np
 import scipy as sp
-import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.ticker as mt
+import imageio
+
 
 class Data:
     def __init__(self):
@@ -16,3 +20,22 @@ class Data:
 
 data = Data()
 data.generate(0.5,150)
+
+
+fig, ax = plt.subplots()
+
+x = np.arange(0, 2*np.pi, 0.01).reshape(-1,1)
+#y = np.linspace(0, 2*np.pi, 120).reshape(-1,1)
+line, = ax.plot(np.sin(x))
+
+def init():
+    line.set_ydata([np.nan] * len(x))
+    return line,
+
+def update(i):
+    line.set_ydata(np.sin(x+i/100))
+    return line,
+
+ani = animation.FuncAnimation(fig, update, init_func = init, interval = 2, blit = True)
+
+plt.show()
